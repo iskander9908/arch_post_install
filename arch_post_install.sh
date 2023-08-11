12d8cbb009cded442daf0076dd812302d5dc3176
iskander9908 <99098033+iskander9908@users.noreply.github.com>
2023-08-11 21:23:56 +0600

    arch_post_install.sh

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-#Wayland and Xwayland
-sudo pacman -S wayland xorg-xwayland
+#Gen rec packs
+sudo pacman -S ufw rsync reflector wayland xorg-xwayland sway swaylock swayidle swaybg otf-font-awesome waybar foot dunst transmission polkit wofi grim slurp sddm xdg-user-dirs tlp pipewire lib32-pipewire pipewire-docs pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack
 
 #Drivers
 sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau mesa-utils
 
-#Window manager and sompe apps
-sudo pacman -S sway swaylock swayidle swaybg otf-font-awesome waybar foot dunst transmission polkit wofi grim slurp
-mkdir -p ~/.config/sway
-cp ~/arch_post_install/sway/config ~/.config/sway
-cp -r ~/arch_post_install/waybar ~/.config/
-
-#Display manager
-sudo pacman -S sddm
-sudo systemctl enable sddm.service
-
 #Security
-sudo pacman -S ufw rsync
 sudo systemctl enable ufw.service
 sudo ufw default deny
 sudo ufw allow from 192.168.0.0/24
@@ -25,20 +14,17 @@ sudo ufw allow Transmission
 sudo ufw limit ssh
 sudo ufw enable
 
+#Display manager
+sudo systemctl enable sddm.service
+
 #Mirrors
-sudo pacman -S reflector
 sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 #Home dirs
-sudo pacman -S xdg-user-dirs
 xdg-user-dirs-update
 
 #Power managment
-sudo pacman -S tlp
 sudo systemctl enable tlp.service
-
-#Sound system
-sudo pacman -S pipewire lib32-pipewire pipewire-jack lib32-pipewire-jack pipewire-docs wireplumber qpwgraph
 
 #Another apps
 sudo pacman -S firefox telegram-desktop spotify-launcher thunar xarchiver catfish featherpad libreoffice-still atril qalculate-gtk
