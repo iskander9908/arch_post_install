362130dc16f9cede0c02d8fcbd9b74b45d0ec2d6
iskander9908 <99098033+iskander9908@users.noreply.github.com>
2023-08-13 16:50:18 +0600

    arch_sway.sh

--- arch_sway.sh
+++ arch_sway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Gen rec packs
-sudo pacman -S ufw rsync reflector wayland xorg-xwayland sway swaylock swayidle swaybg noto-fonts otf-font-awesome waybar foot dunst man-db transmission-qt polkit wofi grim slurp xdg-user-dirs tlp pipewire lib32-pipewire pipewire-docs wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack pavucontrol
+sudo pacman -S ufw rsync reflector wayland xorg-xwayland sway swaylock swayidle swaybg brightnessctl noto-fonts otf-font-awesome waybar foot dunst man-db transmission-qt polkit wofi grim slurp xdg-user-dirs tlp pipewire lib32-pipewire pipewire-docs wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack pavucontrol
 
 #Drivers
 sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau mesa-utils
