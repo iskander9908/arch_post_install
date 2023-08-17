369228fda39f4b226dd71eba690cef39b519ad08
iskander9908 <99098033+iskander9908@users.noreply.github.com>
2023-08-17 03:12:27 +0000

    sway.sh

--- sway.sh
+++ sway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Gen rec packs
-sudo pacman -S ufw rsync reflector wayland xorg-xwayland sway swaylock swayidle swaybg udisks2 brightnessctl noto-fonts otf-font-awesome waybar foot dunst man-db network-manager-applet nm-connection-editor transmission-qt polkit wofi grim slurp xdg-user-dirs tlp pipewire lib32-pipewire pipewire-docs wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack pavucontrol
+sudo pacman -S ufw rsync reflector wayland xorg-xwayland sway swaylock swayidle swaybg udisks2 brightnessctl noto-fonts otf-font-awesome waybar foot mako man-db network-manager-applet nm-connection-editor transmission-qt polkit wofi grim slurp xdg-user-dirs tlp pipewire lib32-pipewire pipewire-docs wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack pavucontrol
 
 # Drivers
 sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau mesa-utils
@@ -26,7 +26,6 @@ sudo systemctl enable tlp.service
 # Sway
 cp -r ~/arch_post_install/sway ~/.config/
 cp -r /etc/xdg/waybar ~/.config/
-cp ~/arch_post_install/wanda-maximoff-scarlet-witch-elizabeth-olsee-5k-ql-1920x1080.jpg ~/Pictures
 
 # Another apps
 sudo pacman -S firefox telegram-desktop vlc spotify-launcher thunar imv xarchiver catfish featherpad libreoffice-still atril qalculate-gtk
