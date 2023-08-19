e45c9aa312a8583f8db1c18dfe355350cd5326b4
iskander9908 <99098033+iskander9908@users.noreply.github.com>
2023-08-20 00:12:28 +0600

    sway.sh

--- sway.sh
+++ sway.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Gen rec packs
-sudo pacman -S ufw rsync reflector wayland xorg-xwayland sway swaylock swayidle swaybg udisks2 brightnessctl noto-fonts otf-font-awesome waybar foot dunst man-db network-manager-applet nm-connection-editor transmission-gtk polkit wofi grim slurp xdg-user-dirs tlp pipewire lib32-pipewire pipewire-docs wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack pavucontrol
+sudo pacman -S ufw rsync reflector wayland xorg-xwayland sway swaylock swayidle swaybg udisks2 brightnessctl noto-fonts otf-font-awesome waybar thunar foot dunst man-db network-manager-applet nm-connection-editor transmission-gtk polkit wofi grim slurp xdg-user-dirs tlp pipewire lib32-pipewire pipewire-docs wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack pavucontrol
 
 # Drivers
 sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau mesa-utils
@@ -35,6 +35,6 @@ cp -r ~/arch_post_install/systemd/ ~/.config/
 cp -r /etc/xdg/foot ~/.config/
 
 # Another apps
-sudo pacman -S firefox telegram-desktop vlc spotify-launcher pcmanfm-gtk3 imv zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps file-roller libreoffice-still qalculate-gtk
+sudo pacman -S firefox telegram-desktop vlc spotify-launcher imv zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps file-roller libreoffice-still qalculate-gtk
 
 ### Also you may want to create swap file, change shell, configure usb mounter and install aur helper
